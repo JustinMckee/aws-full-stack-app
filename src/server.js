fd5e1d89c74b80609dfc18cc3aadf5b07a39c868
Justin McKee <thisisjustinmckee@gmail.com>
2024-01-06 21:01:36 -0500

    Adds express application.

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import bodyParser from "body-parser";
+import express from "express";
+
+(async () => {
+    // Create an Express application.
+    const app = express();
+    // Default port to listen.
+    const port = 8080;
+
+    // Use middleware so post bodies
+    // are accessable as req.body.{{vairable}}
+    app.use(bodyParser.json());
+    app.use(express.urlencoded({extended: true})) // for request from forms-like data
+    
+    // Root URI call
+    app.get("/", (req,res) => {
+        res.status(200).send("Welcome to the Cloud!");
+    });
+
+    app.listen(port, () => {
+        console.log(`Server running http://localhost:${port}`);
+        console.log("Press CTRL+C to stop the server.")
+    });
+
+})();
